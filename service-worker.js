9c704db8aa4c0d7960b615eea968711c820d4046
leeleetw <littlea0315@gmail.com>
2025-05-21 19:12:22 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "counter-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./background.mp3",
+  "./calendar.png",
+  "./back.png",
+  "./playpause.png",
+  "./reset.png",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("message", event => {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
+});
